10016fea7f69722ecfcd59c4ca9231bb6e7d1d8e
Federico Bertolini <fb@hellofresh.com>
2017-02-12 13:08:32 +0100

    exposed services

---
+++ __tests__/index.js
@@ -0,0 +1,48 @@
+const index = require('../src');
+
+it('exports the list of available cities', () => {
+    const cities = index.cities;
+
+    expect(Array.isArray(cities)).toBe(true);
+    expect(cities.length).toBeGreaterThan(10);
+});
+
+it('scraps the first page of the Berlin apartment list and its apartments', () => {
+    const promise = index.scrapCity('Berlin');
+    expect(promise instanceof Promise).toBe(true);
+
+    return promise.then((result) => {
+        expect(Array.isArray(result.items)).toBe(true);
+        expect(result.items.length).toBe(20);
+        expect(result.pagination.page).toBe(1);
+        expect(result.pagination.totalPages).toBeGreaterThan(50);
+
+        result.items.forEach((apartment) => {
+            expect(apartment.rentBase).toBeGreaterThan(200);
+            expect(apartment.area).toBeGreaterThan(15);
+            expect(apartment.rooms).toBeGreaterThan(0);
+            expect(apartment.postalCode).toBeTruthy();
+            expect(apartment.city).toBe('Berlin');
+        });
+    });
+});
+
+it('scraps the second page of the Köln apartment list and its apartments', () => {
+    const promise = index.scrapCity('Köln', 2);
+    expect(promise instanceof Promise).toBe(true);
+
+    return promise.then((result) => {
+        expect(Array.isArray(result.items)).toBe(true);
+        expect(result.items.length).toBe(20);
+        expect(result.pagination.page).toBe(2);
+        expect(result.pagination.totalPages).toBeGreaterThan(5);
+
+        result.items.forEach((apartment) => {
+            expect(apartment.rentBase).toBeGreaterThan(200);
+            expect(apartment.area).toBeGreaterThan(15);
+            expect(apartment.rooms).toBeGreaterThan(0);
+            expect(apartment.postalCode).toBeTruthy();
+            expect(apartment.city.includes('Köln')).toBe(true);
+        });
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const request = require('request');
+const listScraper = require('./scrapers/list');
+const apartmentScraper = require('./scrapers/apartment');
+const cityPaths = require('./cityPaths.json');
+
+const host = 'https://www.immowelt.de';
+
+const getListUrl = (city, page) => {
+    const cityPath = cityPaths[city];
+    if (!cityPath) {
+        throw new Error(`Invalid city: ${city}`);
+    }
+    return `${host}/liste/${cityPath}/wohnungen/mieten?cp=${page}`;
+};
+
+const scrapApartment = url => new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+        if (error) {
+            reject(error);
+        }
+        if (response.statusCode !== 200) {
+            reject(`Invalid response: ${response.statusCode}`);
+        }
+        const apartment = apartmentScraper.scrap(body);
+        apartment.url = url;
+
+        resolve(apartment);
+    });
+});
+
+const scrapCity = (city, page = 1) => new Promise((resolve, reject) => {
+    let url;
+    try {
+        url = getListUrl(city, page);
+    } catch (e) {
+        reject(e);
+        return;
+    }
+
+    request(url, (error, response, body) => {
+        if (error) {
+            reject(error);
+        }
+        if (response.statusCode !== 200) {
+            reject(`Invalid response: ${response.statusCode}`);
+        }
+        const apartments = listScraper.scrap(body);
+        const apartmentPromises = apartments.items.map(apartment => scrapApartment(host + apartment.url));
+
+        resolve(Promise.all(apartmentPromises).then(items => ({
+            items,
+            pagination: apartments.pagination,
+        })));
+    });
+});
+
+exports.cities = Object.keys(cityPaths);
+exports.scrapCity = scrapCity;
